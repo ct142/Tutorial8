ef2d8416921e15260e8d51423a3a10ce160c6ea0
ct142 <102135900+ct142@users.noreply.github.com>
2022-12-13 15:29:42 +0700

    new

--- src/Ex1/UserDataCollection.java
+++ src/Ex1/UserDataCollection.java
@@ -1,7 +1,32 @@
 package Ex1;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class UserDataCollection {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args) throws IOException {
+        // Exercise 1
+        String name, address;
+        int age;
+        Scanner scanner = new Scanner(System.in);
+
+        PrintWriter pw = new PrintWriter(new FileWriter("src/Ex1/users.txt",true));
+
+        System.out.println("Enter name : ");
+        name = scanner.nextLine();
+
+        System.out.println("Enter age : ");
+        age = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter address:");
+        address = scanner.nextLine();
+
+        pw.println(name + "," + age + "," + address);
+
+
+        pw.flush();
+        pw.close();
     }
 }
